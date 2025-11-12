ad9d0de0c1203121cafb5e5279c3fc71b96358aa
Local User <user@local.dev>
2025-11-12 14:14:12 +0100

    Update decorator pattern implementation and add new files

--- decorator_final.py
+++ decorator_final.py
@@ -10,7 +10,6 @@ def make_blink(function):
 	def decorator():
 		#Grab the return value of the function being decorated
 		ret = function() 
-
 		#Add new functionality to the function being decorated
 		return "<blink>" + ret + "</blink>"
 
